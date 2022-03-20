9da1da6b9744df1698a383046495c0e573efdd40
gabrielaugusto123 <84480867+gabrielaugusto123@users.noreply.github.com>
2022-03-20 19:53:57 -0300

    Add files via upload

---
+++ pratica2c.java
@@ -0,0 +1,25 @@
+package pratica2;
+import java.util.Arrays;
+import java.util.Scanner;
+public class pratica2c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner ler = new Scanner(System.in);
+		
+		int[] numeros = new int[10];
+		
+		for(int i=0; i<numeros.length; i++) {
+				System.out.println("Insira os números: ");
+				numeros[i] = ler.nextInt();
+		}
+		
+		Arrays.sort(numeros);
+
+        System.out.println("Esses sao os números ordenados:");
+        for(int numero : numeros)
+            System.out.println(numero);
+
+	}
+
+}
